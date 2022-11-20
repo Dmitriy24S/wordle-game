5528178d00be526c4ffc25b6fb40ac22ad306ae9
Dmitriy <devilins24@gmail.com>
2022-11-20 10:50:58 +0200

    feat: add word keyboard typing, letter result color
    * get allGuesses, inexactGuesses, exactGuesses for key result color

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,65 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Guess from './components/Guess'
 import Keyboard from './components/Keyboard'
 
 function App() {
-  const guess = 'guesh'
   const word = 'ghost'
+  const [guess, setGuess] = useState<string>('')
+  const [guessWordList, setGuessWordList] = useState<string[]>((new Array(6).fill('')))
+  const [guessAttemptNumber, setGuessAttemptNumber] = useState(0)
+
+  useEffect(() => {
+    const keyPress = (e: KeyboardEvent) => {
+      // letter press
+      if (e.key.match(/^[A-z]$/) && guess.length < 5) {
+        // console.log('e.key', e.key);
+        setGuess((prev) => prev + e.key.toLowerCase())
+        // console.log('guess.length', guess.length);
+      }
+      // if enter -> submit word as guess
+      if (e.key === 'Enter' && guess.length === 5) {
+        // console.log('enter enter?', 'guessAttemptNumber', guessAttemptNumber); // 0
+        setGuessWordList((prev) => prev.map((word, index) => index === guessAttemptNumber ? guess : word
+        ))
+        // console.log('guessWordList', guessWordList); // empty -> delay?
+        setGuessAttemptNumber((prev) => prev + 1)
+        setGuess('')
+      }
+      // backspace -> remove last letter from current guess word
+      if (e.key === 'Backspace') {
+        setGuess((currentGuess) => currentGuess.slice(0, currentGuess.length - 1))
+      }
+    }
+
+    document.body.addEventListener('keydown', keyPress)
+    // cleanup
+    return () => {
+      document.body.removeEventListener('keydown', keyPress)
+    }
+  }, [guess, guessWordList, guessAttemptNumber])
+
 
   return (
-    <>
-      <div className="App">
-        <h1 className='title'>Wordle Clone</h1>
-        <div className="game">
-          {[...Array(6)].map((item, index) => (
-            <Guess word={word} guess={guess} key={index} />
-          ))}
-        </div>
-        <Keyboard word={word} guess={guess} />
+    <div className="App">
+      <h1 className='title'>Wordle Clone</h1>
+      <div className="game">
+        {[...Array(6)].map((_item, index) => (
+          <Guess
+            word={word}
+            guess={guess}
+            key={index}
+            guessWordList={guessWordList}
+            row={index}
+            guessAttemptNumber={guessAttemptNumber}
+          />
+        ))}
       </div>
-    </>
+      <Keyboard
+        word={word}
+        guessWordList={guessWordList}
+        guessAttemptNumber={guessAttemptNumber}
+      />
+    </div>
   )
 }
 
--- src/components/Guess.tsx
+++ src/components/Guess.tsx
@@ -3,15 +3,32 @@ import React from 'react'
 interface Props {
     word: string
     guess: string
+    guessWordList: string[]
+    row: number
+    guessAttemptNumber: number
 }
 
-const Guess = ({ word, guess }: Props) => {
+const Guess = ({ word, guess, guessWordList, row, guessAttemptNumber }: Props) => {
+    // console.log('guess', guess);
+    // console.log('guessWordList', guessWordList);
+    // guessWordList (6) ['', '', '', '', '', '']
+
+    // guessWordList(6)['snowy', '', '', '', '', '']
+    // word ghost
+    // allGuesses(5)['s', 'n', 'o', 'w', 'y']
+    // inexactGuesses(2)['o', 's']
+    // exactGuesses['o']
+
     return (
         <div className='guess-row'>
-            {[...Array(5)].map((item, index) => {
-                const letterStatus = guess[index] === word[index] ? 'correct' : word.includes(guess[index]) ? 'wrong-order' : 'wrong'
+            {[...Array(5)].map((_item, index) => {
+                // console.log('guessWordList[row]', guessWordList[row]); // empty
+                const letterStatus = guessWordList[row][index] === word[index] ? 'correct' : word.includes(guessWordList[row][index]) ? 'wrong-order' : 'wrong'
+
                 return (
-                    <div className={`guess-square ${letterStatus}`} key={index}>{guess[index]}</div>
+                    <div className={`guess-square ${letterStatus}`} key={index}>
+                        {guessAttemptNumber === row ? guess[index] : guessWordList[row][index]}
+                    </div>
                 )
             })}
         </div>

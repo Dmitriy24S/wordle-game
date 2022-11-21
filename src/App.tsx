8e2d2eaf3988625cb81eb09ce8222476b6e81d4e
Dmitriy <devilins24@gmail.com>
2022-11-21 15:03:36 +0200

    feat: add handleKeyClick for game keyboard, refactor letterStatusKeyboard
    * add handleKeyClick to game keyboard with refactored Enter & Backspace func.
    * refactor letterStatusKeyboard (conditional keyboard letter color logic)

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,58 @@ function App() {
   const [guessWordList, setGuessWordList] = useState<string[]>((new Array(6).fill('')))
   const [guessAttemptNumber, setGuessAttemptNumber] = useState(0)
 
+  // Click/tap on screen keyboard:
+  const handleKeyClick = (e: React.MouseEvent) => {
+    // const handleKeyClick = (e: Event) => {
+    console.log('guess', guess);
+    // console.log(e);
+    // console.log(e.target); // <button class="keyboard-key ">u</button>
+    // console.log(e.target.textContent);
+    const target = e.currentTarget as HTMLButtonElement;
+    // const target = e.target as HTMLButtonElement;
+    if (guess.length < 5) {
+      setGuess((prev) => prev + target.textContent)
+      return
+    }
+    if (target.textContent === 'Enter') {
+      handleEnter()
+      return
+    }
+    if (target.ariaLabel === 'backspace') {
+      console.log('backspace', 111111);
+      handleBackspace()
+      return
+    }
+  }
+
+  const handleEnter = () => {
+    console.log('HANDLE ENTER guess length', guess.length);
+    if (guess.length < 5) return
+    setGuessWordList((prev) => prev.map((word, index) => index === guessAttemptNumber ? guess : word
+    ))
+    setGuessAttemptNumber((prev) => prev + 1)
+    setGuess('')
+    // TODO: calc GameResult
+  }
+
+  const handleBackspace = () => {
+    setGuess((currentGuess) => currentGuess.slice(0, currentGuess.length - 1))
+  }
+
+  // guessWordList(6)['snowy', '', '', '', '', '']
+  // word ghost
+  // allGuesses(5)['s', 'n', 'o', 'w', 'y']
+  // inexactGuesses(2)['o', 's']
+  // exactGuesses['o']
+  console.log('guessWordList', guessWordList);
+  // guessWordList (6) ['ghost', '', '', '', '', '']
+  console.log('word', word);
+  // word ghost
+
+  // Keyboard press typing
   useEffect(() => {
     const keyPress = (e: KeyboardEvent) => {
+      console.log('keyPress e', e);
       // letter press
       if (e.key.match(/^[A-z]$/) && guess.length < 5) {
         // console.log('e.key', e.key);
@@ -18,19 +68,13 @@ function App() {
       }
       // if enter -> submit word as guess
       if (e.key === 'Enter' && guess.length === 5) {
-        // console.log('enter enter?', 'guessAttemptNumber', guessAttemptNumber); // 0
-        setGuessWordList((prev) => prev.map((word, index) => index === guessAttemptNumber ? guess : word
-        ))
-        // console.log('guessWordList', guessWordList); // empty -> delay?
-        setGuessAttemptNumber((prev) => prev + 1)
-        setGuess('')
+        handleEnter()
       }
       // backspace -> remove last letter from current guess word
       if (e.key === 'Backspace') {
-        setGuess((currentGuess) => currentGuess.slice(0, currentGuess.length - 1))
+        handleBackspace()
       }
     }
-
     document.body.addEventListener('keydown', keyPress)
     // cleanup
     return () => {
@@ -38,6 +82,34 @@ function App() {
     }
   }, [guess, guessWordList, guessAttemptNumber])
 
+  // Key/letter status color:
+  const allGuesses = guessWordList.slice(0, guessAttemptNumber).join('').split('')
+  console.log('allGuesses', allGuesses);
+  // Keyboard.tsx:47 allGuesses (5) ['g', 'u', 'e', 's', 's']
+
+  const inexactGuesses = word
+    .split('')
+    .filter((letter) => allGuesses.includes(letter))
+  console.log('inexactGuesses', inexactGuesses);
+  // inexactGuesses (2) ['g', 's']
+
+  const exactGuesses = word
+    .split('')
+    // if any guesses include this letter in this position/index
+    .filter((letter, i) => {
+      return guessWordList
+        .slice(0, guessAttemptNumber)
+        .map((word) => word[i])
+        .includes(letter)
+    })
+  console.log('exactGuesses', exactGuesses);
+  // exactGuesses (5) ['g', 'h', 'o', 's', 't']
+
+  const letterStatusKeyboard = (letter: string) => {
+    return exactGuesses.includes(letter) ? 'correct'
+      : inexactGuesses.includes(letter) ? 'wrong-order'
+        : allGuesses.includes(letter) ? 'wrong' : ''
+  }
 
   return (
     <div className="App">
@@ -55,9 +127,8 @@ function App() {
         ))}
       </div>
       <Keyboard
-        word={word}
-        guessWordList={guessWordList}
-        guessAttemptNumber={guessAttemptNumber}
+        handleKeyClick={handleKeyClick}
+        letterStatusKeyboard={letterStatusKeyboard}
       />
     </div>
   )
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -7,51 +7,11 @@ const QWERTY_KEYS = [
 ]
 
 interface Props {
-    word: string
-    guessWordList: string[]
-    guessAttemptNumber: number
+    handleKeyClick: (e: React.MouseEvent) => void
+    letterStatusKeyboard: (key: string) => void
 }
 
-const Keyboard = ({ word, guessWordList, guessAttemptNumber }: Props) => {
-    // guessWordList(6)['snowy', '', '', '', '', '']
-    // word ghost
-    // allGuesses(5)['s', 'n', 'o', 'w', 'y']
-    // inexactGuesses(2)['o', 's']
-    // exactGuesses['o']
-
-    console.log('guessWordList', guessWordList);
-    // guessWordList (6) ['ghost', '', '', '', '', '']
-    console.log('word', word);
-    // word ghost
-
-    const allGuesses = guessWordList.slice(0, guessAttemptNumber).join('').split('')
-    console.log('allGuesses', allGuesses);
-    // Keyboard.tsx:47 allGuesses (5) ['g', 'u', 'e', 's', 's']
-
-    const inexactGuesses = word
-        .split('')
-        .filter((letter) => allGuesses.includes(letter))
-    console.log('inexactGuesses', inexactGuesses);
-    // inexactGuesses (2) ['g', 's']
-
-    const exactGuesses = word
-        .split('')
-        // if any guesses include this letter in this position/index
-        .filter((letter, i) => {
-            return guessWordList
-                .slice(0, guessAttemptNumber)
-                .map((word) => word[i])
-                .includes(letter)
-        })
-    console.log('exactGuesses', exactGuesses);
-    // exactGuesses (5) ['g', 'h', 'o', 's', 't']
-
-    const keyStatus = (key: string) => {
-        return exactGuesses.includes(key) ? 'correct'
-            : inexactGuesses.includes(key) ? 'wrong-order'
-                : allGuesses.includes(key) ? 'wrong' : ''
-    }
-
+const Keyboard = ({ handleKeyClick, letterStatusKeyboard }: Props) => {
     return (
         <div className='keyboard'>
             {QWERTY_KEYS.map((row, index) => {
@@ -60,9 +20,9 @@ const Keyboard = ({ word, guessWordList, guessAttemptNumber }: Props) => {
                     return (
                         <div className='keyboard-row' key={row}>
                             {row.split('').map((key) => {
-                                const status = keyStatus(key)
+                                const status = letterStatusKeyboard(key)
                                 return (
-                                    <button className={`keyboard-key ${status}`} key={key}>
+                                    <button className={`keyboard-key ${status}`} key={key} onClick={handleKeyClick}>
                                         {key}
                                     </button>
                                 )
@@ -76,9 +36,9 @@ const Keyboard = ({ word, guessWordList, guessAttemptNumber }: Props) => {
                         <div className='keyboard-row' key={row}>
                             <div className="keyboard-space"></div>
                             {row.split('').map((key) => {
-                                const status = keyStatus(key)
+                                const status = letterStatusKeyboard(key)
                                 return (
-                                    <button className={`keyboard-key ${status}`} key={key}>
+                                    <button className={`keyboard-key ${status}`} key={key} onClick={handleKeyClick}>
                                         {key}
                                     </button>
                                 )
@@ -91,16 +51,24 @@ const Keyboard = ({ word, guessWordList, guessAttemptNumber }: Props) => {
                 if (index === 2) {
                     return (
                         <div className='keyboard-row' key={row}>
-                            <button className='keyboard-key enter-key'>Enter</button>
+                            <button className='keyboard-key enter-key' onClick={handleKeyClick}>
+                                Enter</button>
                             {row.split('').map((key) => {
-                                const status = keyStatus(key)
+                                const status = letterStatusKeyboard(key)
                                 return (
-                                    <button className={`keyboard-key ${status}`} key={key}>
+                                    <button className={`keyboard-key ${status}`} key={key} onClick={handleKeyClick}>
                                         {key}
                                     </button>
                                 )
                             })}
-                            <button aria-label='backspace' className='keyboard-key backspace-key'>
+                            <button aria-label='backspace' className='keyboard-key backspace-key'
+                                // onClick={(e) => {
+                                //     console.log(e, 'backspace e');
+                                //     console.log(e.target, 'backspace target e');
+                                //     console.log(e.currentTarget, 'backspace e currentTarget');
+                                //     handleKeyClick(e)
+                                // }}
+                                onClick={handleKeyClick} >
                                 <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" className="game-icon" data-testid="icon-backspace"><path fill="white" d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"></path></svg>
                             </button>
                         </div>
